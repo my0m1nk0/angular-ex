75ab8fe04b81c3e30a409076987f4477d25be152
my0m1nk0 <myominko.msdev@gamil.com>
2023-06-24 20:03:00 +0630

    event

--- src/app/component_ex/databinding/databinding.component.ts
+++ src/app/component_ex/databinding/databinding.component.ts
@@ -6,5 +6,28 @@ import { Component } from '@angular/core';
   styleUrls: ['./databinding.component.css']
 })
 export class DatabindingComponent {
+changeRange(num: string) {
+  this.value=Number(num);
+}
+
+
+   value=100;
+  bgColor:string='#ff33ff'
+   getColor(str:string){
+    this.bgColor = str;
+    
+
+   }
+
+   
+   
+   changeBackground():string{
+    console.log(this.bgColor);
+    
+    
+    return "background-color:" + this.bgColor + ";";
+}
+
+   
 
 }
